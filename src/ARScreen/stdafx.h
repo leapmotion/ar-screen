2d1a77178c45ee032cca65ddd8eda569a13148d8
Raffi Bedikian <rbedikian@leapmotion.com>
2015-06-04 15:06:35 -0700

    Add a bunch of stuff to stdafx

--- src/ARScreen/stdafx.h
+++ src/ARScreen/stdafx.h
@@ -1,12 +1,55 @@
 #pragma once
 
+// stl
 #define _USE_MATH_DEFINES
 #include <cmath>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <cassert>
+#include <stdexcept>
+#include <iostream>
+#include <algorithm>
+#include <memory>
+#include <string>
 
+// autowiring
 #include <autowiring/autowiring.h>
 
+// windows-specific
 #ifdef _MSC_VER
-  #include <Windows.h>
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#include <Windows.h>
+#include <dwmapi.h>
 #undef min //WTF windows.h
 #undef max
 #endif
+
+// opengl
+#if __APPLE__
+#include <GL/glew.h>
+#elif _WIN32
+#define GLEW_STATIC
+#include <GL/glew.h>
+#endif
+
+// sfml
+#include <SFML/Window.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+
+// leap
+#include <Leap.h>
+
+// oculus
+#include <OVR.h>
+#include <OVR_Kernel.h>
+
+// freeimage
+#define FREEIMAGE_LIB
+#include <FreeImage.h>

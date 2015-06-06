b4311fe4002a311ca39a84e60a8582e7d85e52a0
Raffi Bedikian <rbedikian@leapmotion.com>
2015-06-06 16:48:40 -0700

    Fix globals linking

--- src/ARScreen/Globals.cpp
+++ src/ARScreen/Globals.cpp
@@ -1,2 +1,10 @@
 #include "stdafx.h"
 #include "Globals.h"
+
+bool Globals::haveOculus = false;
+std::chrono::steady_clock::time_point Globals::prevFrameTime;
+std::chrono::steady_clock::time_point Globals::curFrameTime;
+std::chrono::duration<double> Globals::timeBetweenFrames;
+Eigen::Vector3d Globals::userPos(0, 150, 400);
+Eigen::Vector3d Globals::offset(0, 0, 0);
+Eigen::Vector3d Globals::glowColor(0.7, 0.9, 1.0);
--- src/ARScreen/Globals.h
+++ src/ARScreen/Globals.h
@@ -1,10 +1,13 @@
 #pragma once
 #include <chrono>
 
-namespace Globals {
-  static bool haveOculus = false;
+class Globals {
+public:
+  static bool haveOculus;
   static std::chrono::steady_clock::time_point prevFrameTime;
   static std::chrono::steady_clock::time_point curFrameTime;
   static std::chrono::duration<double> timeBetweenFrames;
-  static Eigen::Vector3d userPos(0, 150, 400);
+  static Eigen::Vector3d userPos;
+  static Eigen::Vector3d offset;
+  static Eigen::Vector3d glowColor;
 };
